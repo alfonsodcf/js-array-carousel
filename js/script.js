e259696ed1a12647bd256616046300cfb99df68b
alfonsodcf <alfonsoservizi01@gmail.com>
2022-04-01 09:36:35 +0200

    script.active

--- js/script.js
+++ js/script.js
@@ -36,15 +36,28 @@ let thumpTemplate = '';
     //.next .fa-circle-chevron-down
     //.prev .fa-circle-chevron-up
     //active
-
+let currentIndexActive = 0;
 
 for(let i = 0; i < items.length; i++){
+    let classActive = 'item';
+    if(i === urrentIndexActive ) {
+        classActive = "item active";
+    }
     itemTemplate += `
-        <div class="item active">
+        <div class="item">
            <img src="${items[i]}">
             <div class="title">
                 <h2>${title[i]}</h2>
                 <p>${text[i]}</p>
             </div>
         </div>`;
+
+//console.log(itemTemplate);
 }
+
+//console.log(itemTemplate);
+const itemContainer = document.querySelector(".items-container");
+//console.log(itemContainer);
+itemContainer.innerHTML = itemTemplate;
+let firstItem = document.querySelector(".item");
+firstItem.classList.add("active");
